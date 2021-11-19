b58c1d19361c1a4c59bf0fc7f08bb169c7990b88
angusrock <cazon.juancruz@gmail.com>
2021-11-19 02:20:39 -0300

    ItemCount feature

---
+++ src/components/ItemCount.js
@@ -0,0 +1,38 @@
+import './ItemCount.css';
+import {useState} from 'react';
+
+function ItemCount(props){
+    const [count, setCount] = useState(parseInt(props.initial));
+    const [stock, setStock] = useState(parseInt(props.stock));
+
+    function increment(){
+        if(count < props.stock) setCount(count + 1);        
+    }
+    function decrement(){
+        if(count > 0) setCount(count - 1);        
+    }
+
+    function onAdd(){
+        setStock(stock - count);
+    }
+
+    return(
+        <div className="d-flex justify-content-center">
+            <div className="box products">
+                <h4>Bolsas de Café</h4>            
+                <div className="col-xs-2">
+                    <div className="price">Stock Disponible: {stock > 0 ? stock : <b>Sin Stock</b>}</div>
+                    <p className="inline mr">Cantidad:</p>
+                    <button className="btn-primary btn-sm" onClick={decrement}>-</button>
+                    <p className="inline mr ml">{count}</p>
+                    <button className="btn-primary btn-sm" onClick={increment}>+</button>
+                </div>
+                <div className="mt-3">
+                    <button type="button" className="btn btn-primary" onClick={onAdd}>Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,13 @@
-export default function GreetingsMsg(props){
+import ItemCount from './ItemCount';
+
+const ItemListContainer = (props) => {
     return (
         <>
-        <h1 class="d-flex justify-content-center mt-4">{props.title}</h1>
-        <h4 class="d-flex justify-content-center">{props.greeting}</h4>
+        <h1 className="d-flex justify-content-center mt-4">{props.title}</h1>
+        <h4 className="d-flex justify-content-center">{props.greeting}</h4>
+        <ItemCount initial="1" stock="7"/>                        
         </>
-    );
+    )
 }
+
+export default ItemListContainer;
