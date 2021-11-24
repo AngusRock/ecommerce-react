d8af26a090ac22a0f45831b8c136d1d87b5b4184
angusrock <cazon.juancruz@gmail.com>
2021-11-24 11:57:20 -0300

    added item description

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,6 +5,7 @@ export default function Item(item){
         <div className="d-flex justify-content-center">
         <div className="box products">
             <h4>{item.title}</h4>
+            <h6>{item.description}</h6>
             <img src={item.pictureUrl} alt="icono cafe" />
             <ItemCount initial="1" stock={item.stock} />
             <div className="mt-3">
