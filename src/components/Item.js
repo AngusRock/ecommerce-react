d2f7cc7c3cabd867b36c949dd4b3cf375b03e340
angusrock <cazon.juancruz@gmail.com>
2021-11-24 12:01:02 -0300

    added price

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,8 +5,8 @@ export default function Item(item){
         <div className="d-flex justify-content-center">
         <div className="box products">
             <h4>{item.title}</h4>
-            <h6>{item.description}</h6>
-            <img src={item.pictureUrl} alt="icono cafe" />
+            <h6>{item.description} (${item.price})</h6>
+            <img src={item.pictureUrl} alt="icono cafe" />            
             <ItemCount initial="1" stock={item.stock} />
             <div className="mt-3">
             <button type="button" className="btn btn-secondary btn-sm">Ver Detalle Producto</button>
