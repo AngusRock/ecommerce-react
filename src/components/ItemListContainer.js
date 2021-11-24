9a314b1844e1e2c58c866589505b21311d916cf2
angusrock <cazon.juancruz@gmail.com>
2021-11-24 15:53:45 -0300

    improve loading validation

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -5,8 +5,10 @@ import './ItemListContainer.css';
 
 const ItemListContainer = (props) => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     function getData(timeOut){
+        setLoading(true);        
         return new Promise((resolve, reject) => {
             if (data.length > 0) {
                 setTimeout(() => {
@@ -18,20 +20,21 @@ const ItemListContainer = (props) => {
         });
     }
 
-    useEffect(() => {
+    useEffect(() => {        
         getData(2000)
         .then(response => {
+            setLoading(false);            
             setProducts(response);
         })
         .catch(error => console.log('ocurrio un error'));
-    })
+    }, []);
 
     return (
-        <>
+        <>        
         <h1 className="d-flex justify-content-center mt-4">{props.title}</h1>
         <h4 className="d-flex justify-content-center">{props.greeting}</h4><br/>
         <div className="box-container">
-            {products.length > 0 ? <ItemList items={products} /> : <h5 className="d-flex justify-content-center">Cargando datos...</h5>}
+            {loading ? <h5 className="d-flex justify-content-center">Cargando datos...</h5> : <ItemList items={products} />}
         </div>
         </>
     )
